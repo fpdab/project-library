166afe03489e45152be5ae1f7214093d308de126
fpdab <155249090+fpdab@users.noreply.github.com>
2024-02-24 19:45:07 +0100

    refactored code into classes

--- script.js
+++ script.js
@@ -4,44 +4,26 @@ class BookLibrary {
   }
 
   addBookToLibrary(title, author, pages, read) {
-    this.bookLib.push(new SingleBook(title, author, pages, read));
+    this.bookLib.push(new Book(title, author, pages, read));
   }
-  render() {
-    //Count Cards
-    let counter = 0;
 
-    //Get and clear display before rendering Cards
+  render() {
+    //Clear display before rendering Cards
     let display = document.querySelector(".yourLib");
     while (display.firstChild) {
       display.removeChild(display.firstChild);
     }
 
     //Create Cards and append them to display
-    for (let book of this.bookLib) {
-      const aCard = book.createCard(counter);
+    for (const [index, book] of this.bookLib.entries()) {
+      book.createCard(index);
+      const aCard = book.aCard;
       display.appendChild(aCard);
     }
-
-    /*   let delbuts = document.querySelectorAll(".delb");
-    delbuts.forEach((book) => {
-      let x = book.getAttribute("class").replace(/[^\d-]/g, "");
-      book.addEventListener("click", () => {
-        this.bookLib.splice(Number(x), 1);
-        render();
-      });
-    });
-    let statbuts = document.querySelectorAll(".statb");
-    statbuts.forEach((book) => {
-      let x = book.getAttribute("class").replace(/[^\d-]/g, "");
-      book.addEventListener("click", () => {
-        this.bookLib[Number(x)].flipStatus();
-        render();
-      });
-    }); */
   }
 }
 
-class SingleBook {
+class Book {
   constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -57,80 +39,115 @@ class SingleBook {
   get getPages() {
     return this.pages;
   }
-  get ifRead() {
+  get getReadOrNotRead() {
     return this.read === true ? `read` : "not read";
   }
   flipStatus() {
     this.read === true ? (this.read = false) : (this.read = true);
   }
 
-  createCard(counter) {
-    function createSentence(type, sentenceClass, description, formValue) {
+  createCard(index) {
+    function createCardsElement(type, sentenceClass, description, formValue) {
       const sentence = document.createElement(type);
       sentence.setAttribute("class", sentenceClass);
       const node = document.createTextNode(`${description} ${formValue}`);
       sentence.appendChild(node);
       return sentence;
     }
-
+    //Create card container
+    //All the next paragraphs will be appended to it
     const divText = document.createElement("div");
     divText.setAttribute("class", `text`);
 
-    divText.appendChild(createSentence("p", `title`, `Title:`, this.getTitle));
+    //Add first 3 paragraphs to the card
     divText.appendChild(
-      createSentence("p", `author`, `Author:`, this.getAuthor)
+      createCardsElement("p", `title`, `Title:`, this.getTitle)
     );
-    divText.appendChild(createSentence("p", `pages`, `Pages:`, this.getPages));
+    divText.appendChild(
+      createCardsElement("p", `author`, `Author:`, this.getAuthor)
+    );
+    divText.appendChild(
+      createCardsElement("p", `pages`, `Pages:`, this.getPages)
+    );
 
-    const statusPara = createSentence("p", `read`, `Status:`, ``);
+    //Create mutable Status Paragraph with ability to change colors
+    const statusPara = createCardsElement("p", `read`, `Status:`, ``);
     statusPara.appendChild(
-      createSentence("span", this.ifRead, ``, this.ifRead)
+      createCardsElement(
+        "span",
+        this.getReadOrNotRead,
+        ``,
+        this.getReadOrNotRead
+      )
     );
     divText.appendChild(statusPara);
 
-    const buttonPara = createSentence("p", `buts`, ``, ``);
-    const statButton = createSentence(
+    //Create button paragrraph container
+    const buttonPara = createCardsElement("p", `buts`, ``, ``);
+
+    //Create button for changing Status Paragraph using flipStatus method
+    const statButton = createCardsElement(
       "button",
-      `statb statb${counter}`,
+      `statb statb${index}`,
       ``,
       `Switch Status`
     );
+    statButton.addEventListener("click", () => {
+      this.flipStatus();
+      mainLibrary.render();
+    });
     buttonPara.appendChild(statButton);
-    const delButton = createSentence(
+
+    //Create button for deleting this card
+    const delButton = createCardsElement(
       "button",
-      `delb delb${counter}`,
+      `delb delb${index}`,
       ``,
       `Delete`
     );
+    delButton.addEventListener("click", () => {
+      mainLibrary.bookLib.splice(index, 1);
+      mainLibrary.render();
+    });
     buttonPara.appendChild(delButton);
+
+    //Append buttons to paragraph container
     divText.appendChild(buttonPara);
 
+    //Create aCard container
+    //for divText container which carries all the previous html code
     const aCard = document.createElement("div");
-    aCard.setAttribute("class", `card card${counter}`);
-    counter++;
+    aCard.setAttribute("class", `card card${index}`);
     aCard.appendChild(divText);
+
+    //aCard property stores finished card html code
     this.aCard = aCard;
-    console.log(typeof divText);
   }
 }
 
-// get DOM
-const mainLibrary = new BookLibrary();
-let modal = document.querySelector(".modal");
-document
-  .querySelector(".newBookButton")
-  .addEventListener("click", () => modal.showModal());
-document.querySelector("#form").addEventListener("submit", getForm);
-
+//Function for getting form values, adding book to mainLibrary and then rendering
 function getForm() {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
-  const read = document.getElementById("read").value === "true";
+  const read = document.getElementById("read").value === "true" ? true : false;
 
   mainLibrary.addBookToLibrary(title, author, pages, read);
   mainLibrary.render();
 }
 
+//Script starts here :
+
+//Initialize main library
+const mainLibrary = new BookLibrary();
+
+//Listen for newBookButton click showing dialog
+//Then listen for submit to add book to mainLibrary using getForm function
+let modal = document.querySelector(".modal");
+const newBookButton = document.querySelector(".newBookButton");
+newBookButton.addEventListener("click", () => modal.showModal());
+document.querySelector("#form").addEventListener("submit", getForm);
+
+//Fill page with expample card
 mainLibrary.addBookToLibrary("example", "example", 10, true);
 mainLibrary.render();
